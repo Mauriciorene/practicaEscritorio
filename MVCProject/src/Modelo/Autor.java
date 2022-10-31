a9769f418fe9a291a300717d51282046a4815ec7
Mauricio Rubio <rubiom831@gmail.com>
2022-10-30 18:56:21 -1200

    completo2.

--- MVCProject/src/Modelo/Autor.java
+++ MVCProject/src/Modelo/Autor.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Modelo;
 
 import java.sql.Date;
 /**
- *
  * @author Mauricio Rubio
  */
 public class Autor {
@@ -36,5 +30,53 @@ public class Autor {
         this.Apellidos = Apellidos;
         this.email = email;
         this.fechaNac = fechaNac;     
+    }
+    
+    public int getId_autor() {
+        return id_autor;
+    }
+    
+    public void setd_autor(int id_autor) {
+        this.id_autor = id_autor;
+    }
+    
+    public Date getFechaNac() {
+        return fechaNac;
+    }
+    
+    public void setFechaNac(Date fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+    
+    public String getCedula() {
+        return cedula;
+    }
+    
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    
+    public String getNombres() {
+        return nombres;
+    }
+    
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+    
+    public String getApellidos() {
+        return Apellidos;
+    }
+    
+    public void setApellidos(String Apellidos) {
+        this.Apellidos = Apellidos;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
