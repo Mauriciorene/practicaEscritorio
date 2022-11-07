ab180f7f99ee6a40960e29111d07b5a617a55c70
Mauricio Rubio <rubiom831@gmail.com>
2022-11-06 21:38:32 -1200

    DAOAutor.

--- MVCProject/src/Modelo/DAOAutor.java
+++ MVCProject/src/Modelo/DAOAutor.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Modelo;
+import java.sql.Date;
+//import java.util.List;
+import java.util.*;
 
-/**
- *
- * @author Mauricio Rubio
- */
+
+ // @author Mauricio Rubio
 public class DAOAutor {
+    //Método para insertar datos en la BD
+    public Autor Insertar(String cedula, String nombres, String apellidos,
+            String email, java.sql.Date fechaNac) {
+        String transaccion = "INSERT INTO Autor VALUES('"
+                + nombres + "', '"
+                + apellidos + "', '"
+                + email + "', '"
+                + cedula + "', '"
+                + fechaNac + "')";
+        //Llama al método Actualizar ubicado en DateBase.java
+        if (new DataBase().Actualizar(transaccion) > 0) {
+            return new Autor(cedula, nombres, apellidos, email, fechaNac);
+        }
+        return null;
+    }
+    //métod para actualizar un registro en la BD
+    public int Actualizar(int id, String nombres, String apellidos,
+            String email, String cedula, java.sql.Date fechaNac) {
+        
+        String transaccion = "UPDATE Autor SET nombres=´'"
+                + nombres + "', apellidos='"
+                + apellidos + "', email= '"
+                + email + "', fechaNac= '"
+                + fechaNac + "',cedula='"
+                + cedula +"', WHERE id_autor="
+                + id ;
+        
+        return new DataBase().Actualizar(transaccion);
+    }
     
 }
