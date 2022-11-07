e84763fb4426fce7c6c6550192f339c3c3ce7dc4
Mauricio Rubio <rubiom831@gmail.com>
2022-11-06 22:07:32 -1200

    metodos_DAOAutor 2.

--- MVCProject/src/Modelo/DAOAutor.java
+++ MVCProject/src/Modelo/DAOAutor.java
@@ -35,5 +35,29 @@ public class DAOAutor {
         
         return new DataBase().Actualizar(transaccion);
     }
+    //Método para seleccionar todos los registros de la tabla
+    public List ObtenerDatos() {
+        String transaccion = "SELECT * FROM Autor";
+        //Llama a método Listar de DataBase.java
+        List<Map> registros = new DataBase().Listar(transaccion);
+        List<Autor> autores = new ArrayList(); //Arreglo de Autores
+        //Ciclo que recorre cada registro y los agrega al arreglo autores
+        for (Map registro : registros) {
+            Autor aut = new Autor((int) registro.get("id_autor"),
+                    (String) registro.get("nombres"),
+                    (String) registro.get("apellidos"),
+                    (String) registro.get("email"),
+                    (String) registro.get("cedula"),
+                    (java.sql.Date) registro.get("fechaNac"));
+            autores.add(aut);
+        }
+        return autores; //Retorna todos los autores ubicados en la tabla de BD
+    }
+    //Método para eliminar un registro de la tabla en la BD
+    public int Eliminar(int id) {
+        String transaccion = "DELETE FROM Autor WHERE id_autor=´'"+ id +"'";
+        
+        return new DataBase().Actualizar(transaccion);
+    }
     
 }
